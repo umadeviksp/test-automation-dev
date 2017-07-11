705bcdb498a49578bb5cbe89e55a2849d932194e
TwoTau <TwoTau@users.noreply.github.com>
2017-07-11 13:28:46 -0700

    Change BVT case names

--- bloomz-ui-automation/src/test/java/com/net/bloomz/tests/BV_TestCases.java
+++ bloomz-ui-automation/src/test/java/com/net/bloomz/tests/BV_TestCases.java
@@ -10,30 +10,29 @@ import com.net.bloomz.pages.LandingPage;
 
 public class BV_TestCases extends BaseTest {
 
-	@Test(groups = { "android", "BVT01" })
-	public void BVT01_testAppInstallation() {
+	@Test(groups = { "android", "BVT0101" })
+	public void testAppInstallation() {
 		Assert.assertTrue(((AndroidDriver<?>) browser.getWebDriver()).isAppInstalled("net.bloomz"),
 				"App is not Installed");
 	}
 
-	@Test(groups = { "android", "ios", "BVT02" })
-	public void BVT02_testAppLauch() {
+	@Test(groups = { "android", "ios", "BVT0201" })
+	public void testAppLauch() {
 		LandingPage.getLandingPage(browser).thenVerifySignInAndCreateButtonsShouldBeDisplayed();
 	}
 
-//	@Test(groups = { "android", "ios", "web", "BVT03" })
-//	public void BVT03_testSignInWithValidInputs() throws Exception {
-//
-//		LandingPage.getLandingPage(browser).clickOnSignInButton().enterEmailIdOnTextBox("alphateacher@test.com")
-//				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton().thenVerifyCreateButtonShouldBeDisplayed()
-//				.clickOnSettingButton().clickOnSignOutButton();
-//
-//	}
-	
-	/*
+	@Test(groups = { "android", "ios", "web", "BVT0301" })
+	public void testSignInWithValidInputs() throws Exception {
 
-	@Test(groups = { "android", "ios", "web", "BVT04" })
-	public void BVT04_testSignUpAsATeacher() throws Exception {
+		LandingPage.getLandingPage(browser).clickOnSignInButton().enterEmailIdOnTextBox("alphateacher@test.com")
+				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton().thenVerifyCreateButtonShouldBeDisplayed()
+				.clickOnSettingButton().clickOnSignOutButton();
+
+	}
+	
+	// NEEDS TO BE UPDATED
+	@Test(groups = { "android", "ios", "web", "BVT0401" })
+	public void testSignUpAsATeacher() throws Exception {
 
 		LandingPage.getLandingPage(browser).clickOnCreateAccount().clickOnTeacherIcon()
 				.enterFirstName("test").enterLastName("bloomz")
@@ -42,9 +41,10 @@ public class BV_TestCases extends BaseTest {
 				.clickOnSignOutButton();
 
 	}
-
-	@Test(groups = { "android", "ios", "web", "BVT05" })
-	public void BVT05_testSignUpAsARoomParent() {
+	
+	// NEEDS TO BE UPDATED
+	@Test(groups = { "android", "ios", "web", "BVT0402" })
+	public void testSignUpAsATeacherFRE() throws Exception {
 
 		LandingPage.getLandingPage(browser).clickOnCreateAccount().clickOnRoomParentIcon()
 				.enterFirstName("test").enterLastName("bloomz")
@@ -53,36 +53,60 @@ public class BV_TestCases extends BaseTest {
 				.clickOnSignOutButton();
 	}
 	
-	*/
+	// NEEDS TO BE UPDATED
+	@Test(groups = { "android", "ios", "web", "BVT0403" })
+	public void testSignUpAsTeacherWithInvitationCode() throws Exception {
 
-//	@Test(groups = { "android", "ios", "web", "BVT06" })
-//	public void BVT06_testPostsOnMainFeedForExistingAccount() {
-//
-//		LandingPage.getLandingPage(browser).clickOnSignInButton().enterEmailIdOnTextBox("alphateacher@test.com")
-//				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton().thenVerifyCreateButtonShouldBeDisplayed()
-//				.thenVerifyMainFeedPostShouldNotBeNull().clickOnSettingButton().clickOnSignOutButton();
-//	}
+		LandingPage.getLandingPage(browser).clickOnCreateAccount().clickOnRoomParentIcon()
+				.enterFirstName("test").enterLastName("bloomz")
+				.enterEmailId("test_" + getTimeStamp().replaceAll("-", "_") + "@test.com").enterPassword("bloomz999")
+				.clickOnSignUpButton().thenVerifyCreateButtonShouldBeDisplayed().clickOnSettingButton()
+				.clickOnSignOutButton();
+	}
+	
+	// NEEDS TO BE UPDATED
+	@Test(groups = { "android", "ios", "web", "BVT0404" })
+	public void testSignUpAsARoomParent() throws Exception {
 
-//	@Test(groups = { "android", "ios", "web", "BVT07" })
-//	public void BVT07_testCalendarEventsOnMainFeedForExistingAccount() {
-//		LandingPage.getLandingPage(browser).clickOnSignInButton().enterEmailIdOnTextBox("alphateacher@test.com")
-//				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton().clickOnMyCalendarTab()
-//				.thenVerifyCalendarEventsShouldNotBeNull();
-//		HomePage.getHomePage(browser).clickOnSettingButton().clickOnSignOutButton();
-//	}
+		LandingPage.getLandingPage(browser).clickOnCreateAccount().clickOnRoomParentIcon()
+				.enterFirstName("test").enterLastName("bloomz")
+				.enterEmailId("test_" + getTimeStamp().replaceAll("-", "_") + "@test.com").enterPassword("bloomz999")
+				.clickOnSignUpButton().thenVerifyCreateButtonShouldBeDisplayed().clickOnSettingButton()
+				.clickOnSignOutButton();
+	}
+	
+	// NEEDS TO BE UPDATED
+	@Test(groups = { "android", "ios", "web", "BVT0405" })
+	public void testSignUpAsARoomParentFRE() throws Exception {
 
-	public void BVT08_testMessagesOnMainFeedForExistingAccount() {
-		LandingPage.getLandingPage(browser).clickOnSignInButton().enterEmailIdOnTextBox("alphateacher@test.com")
-				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton();
-
+		LandingPage.getLandingPage(browser).clickOnCreateAccount().clickOnRoomParentIcon()
+				.enterFirstName("test").enterLastName("bloomz")
+				.enterEmailId("test_" + getTimeStamp().replaceAll("-", "_") + "@test.com").enterPassword("bloomz999")
+				.clickOnSignUpButton().thenVerifyCreateButtonShouldBeDisplayed().clickOnSettingButton()
+				.clickOnSignOutButton();
 	}
 
-	@Test(groups = { "android", "ios", "web", "BVT09" })
-	public void BVT09_testVolunteersOnMainFeedForExistingAccount() {
+	@Test(groups = { "android", "ios", "web", "BVT0501" })
+	public void testPostsOnMainFeedForExistingAccount() throws Exception {
+
 		LandingPage.getLandingPage(browser).clickOnSignInButton().enterEmailIdOnTextBox("alphateacher@test.com")
-				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton().clickOnVolunteersTab()
-				.thenVerifyAddVolunteerButtonShouldBeDisplayed();
+				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton().thenVerifyCreateButtonShouldBeDisplayed()
+				.thenVerifyMainFeedPostShouldNotBeNull().clickOnSettingButton().clickOnSignOutButton();
+	}
+
+	@Test(groups = { "android", "ios", "web", "BVT0502" })
+	public void testCalendarEventsOnMainFeedForExistingAccount() throws Exception {
+		LandingPage.getLandingPage(browser).clickOnSignInButton().enterEmailIdOnTextBox("alphateacher@test.com")
+				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton().clickOnMyCalendarTab()
+				.thenVerifyCalendarEventsShouldNotBeNull();
 		HomePage.getHomePage(browser).clickOnSettingButton().clickOnSignOutButton();
+	}
+	
+	// NEEDS TO BE IMPLEMENTED FULLY
+	@Test(groups = { "android", "ios", "web", "BVT0503" })
+	public void testMessagesOnMainFeedForExistingAccount() throws Exception {
+		LandingPage.getLandingPage(browser).clickOnSignInButton().enterEmailIdOnTextBox("alphateacher@test.com")
+				.enterPasswordOnTextBox("bloomz999").clickOnSignInButton();
 
 	}
 
