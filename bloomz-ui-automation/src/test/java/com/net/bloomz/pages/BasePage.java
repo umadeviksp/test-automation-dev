46b57f752d4d7838450daf2e7fee578d37f99a3d
Umadevi KASTHURI SANKARAN PILLAI <30361309+umadeviksp@users.noreply.github.com>
2017-12-18 21:39:24 +0530

    minor fix

--- bloomz-ui-automation/src/test/java/com/net/bloomz/pages/BasePage.java
+++ bloomz-ui-automation/src/test/java/com/net/bloomz/pages/BasePage.java
@@ -25,6 +25,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
